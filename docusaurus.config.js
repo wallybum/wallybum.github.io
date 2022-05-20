6d3ec4c79f78fc29975661c29a0dbf5a5e1ada72
wallybum <elvvis93@gmail.com>
2022-05-20 15:28:54 +0900

    title 변경

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'WallyBum',
-  tagline: 'Tech Blog',
+  tagline: 'Tech Blog2',
   url: 'https://wallybum.github.io',
   baseUrl: '/',
   onBrokenLinks: 'warn', // default : throw
